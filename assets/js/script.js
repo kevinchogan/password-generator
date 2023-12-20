b33fffd92bbc227f1e5f5c16cc8b9ea9703ecc82
Kevin Hogan <kchogan@pacbell.net>
2023-12-19 19:43:16 -0800

    added comments to js and did some minor cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,56 @@
 // Assignment code here
 
+/* generatePassword:
+Prompts the user for the length of password as well as the character sets to be used.  
+Returns a password that meets the user's conditions. */
 function generatePassword() {
+  //Defning sets of characters to be used to generate passwords
   const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   const numeric = ["0","1","2","3","4","5","6","7","8","9"];
   const special = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":"];
-  let isValid = true;
-  let numOfChars;
-  let isLC;
-  let isUC;
-  let isNum;
-  let isSpecial;
-  let newPass= "";
-  let index;
+  
+  //# of character variables
+  let isValid = true; //for error checking # of characters
+  let numOfChars; //# of characters in length the password should be
+  
+  //Should we use characters set?
+  let isLC; //lowercase
+  let isUC; //uppercase
+  let isNum; //numbers
+  let isSpecial; //special characters
+  
+  let charCollection = []; //will contain all valid characters
+  let newPass= ""; //will contain new password to be returned
+  let index; //index to charCollection
 
   do {
+    //Prompt the user of rthe number o characters to use.
     numOfChars = window.prompt("How many characters in length would you like your password to be (8 to 128)?");
-
+    // Exit if user cancels
+    if (!numOfChars) {
+      return;
+    }
+    //Check if the user input is in the correct range
     if (numOfChars >= 8 && numOfChars <= 128) {
       isValid = true;
     } else {
       isValid = false;
     }
-
+    //Show an alert if the input is out of range
     if (!isValid) {
       window.alert("Please enter a number between 8 and 128.");
     }
+    //Continue loop until user input is in range
   } while (!isValid);
 
+  //Prompts for user to decide which character sets to use
   isLC = window.confirm("Would you like to use lowercase letters?");
   isUC = window.confirm("Would you like to use uppercase letters?");
   isNum = window.confirm("Would you like to use numbers?");
   isSpecial = window.confirm("Would you like to use special characters (e.g. '$', '%', '/', etc.?");
 
-  var charCollection = [];
+  //Add character sets to uber collection if use requests
   if (isLC) {
     charCollection = charCollection.concat(lowerCase);
   }; 
@@ -46,11 +63,13 @@ function generatePassword() {
   if (isSpecial) {
     charCollection = charCollection.concat(special);
   };
-
+  
+  //Builds the password using random number generator
   for(let i = 0; i < numOfChars; i++) {
     index = Math.floor(Math.random() * charCollection.length);
     newPass = newPass + charCollection[index]
   };
+  //Return password
   return (newPass);
 }
 
@@ -63,7 +82,10 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //Check if password has been defined before updating web page
+  if (password !== undefined) {
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
