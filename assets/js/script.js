b0f59bf648949df50218547c776575bce7d38834
Kevin Hogan <kchogan@pacbell.net>
2023-12-19 19:27:23 -0800

    First working version.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,21 @@
 // Assignment code here
 
 function generatePassword() {
-  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var numeric = ["0","1","2","3","4","5","6","7","8","9"];
-  var special = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":"];
-  var numOfChars;
-  var isLC;
-  var isUC;
-  var isNum;
-  var isSpecial;
+  const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const numeric = ["0","1","2","3","4","5","6","7","8","9"];
+  const special = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":"];
+  let isValid = true;
+  let numOfChars;
+  let isLC;
+  let isUC;
+  let isNum;
+  let isSpecial;
+  let newPass= "";
+  let index;
 
   do {
-    var isValid = true;
-    numOfChars = window.prompt(
-      "How many characters in length would you like your password to be (8 to 128)?"
-    );
-
-    // if (!numOfChars) {
-    //   return;
-    // }
+    numOfChars = window.prompt("How many characters in length would you like your password to be (8 to 128)?");
 
     if (numOfChars >= 8 && numOfChars <= 128) {
       isValid = true;
@@ -36,8 +32,29 @@ function generatePassword() {
   isUC = window.confirm("Would you like to use uppercase letters?");
   isNum = window.confirm("Would you like to use numbers?");
   isSpecial = window.confirm("Would you like to use special characters (e.g. '$', '%', '/', etc.?");
-  return (numOfChars + isLC + isUC + isNum + isSpecial);
+
+  var charCollection = [];
+  if (isLC) {
+    charCollection = charCollection.concat(lowerCase);
+  }; 
+  if (isUC) {
+    charCollection = charCollection.concat(upperCase);
+  };
+  if (isNum) {
+    charCollection = charCollection.concat(numeric);
+  }; 
+  if (isSpecial) {
+    charCollection = charCollection.concat(special);
+  };
+
+  for(let i = 0; i < numOfChars; i++) {
+    index = Math.floor(Math.random() * charCollection.length);
+    newPass = newPass + charCollection[index]
+  };
+  return (newPass);
 }
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
